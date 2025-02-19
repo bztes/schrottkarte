0b5027fac873196e5eed499a3ff1f629f195e35a
Nico Beierle <nico.beierle@gmail.com>
2025-02-19 22:33:12 +0100

    chore: upgrade packages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-  // @ts-expect-error vitest 2 does not support vite 6
   plugins: [sveltekit(), Icons({ compiler: 'svelte' })],
   ssr: {
     noExternal: ['maplibre-gl'],
